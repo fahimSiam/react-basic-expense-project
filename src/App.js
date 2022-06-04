c299e3f85e8a11a296adc01b302e2e2110d0aead
fahimSiam <siamfahim02@gmail.com>
2022-06-05 02:13:29 +0600

    5/6

--- src/App.js
+++ src/App.js
@@ -5,21 +5,26 @@ import ExpanseItem from "./components/ExpenseItem";
 function App() {
   const expenses = [
     {
-      id: 'e1',
-      title: 'Toilet Paper',
+      id: "e1",
+      title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: 'e3',
-      title: 'Car Insurance',
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
+      id: "e4",
+      title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
@@ -38,7 +43,26 @@ function App() {
           Learn React
         </a> */}
         <h1>Expense Tracker</h1>
-        <ExpanseItem></ExpanseItem>
+        <ExpanseItem
+          title={expenses[0].title}
+          amount={expenses[0].amount}
+          date={expenses[0].date}
+        ></ExpanseItem>
+        <ExpanseItem
+          title={expenses[2].title}
+          amount={expenses[2].amount}
+          date={expenses[2].date}
+        ></ExpanseItem>
+        <ExpanseItem
+          title={expenses[1].title}
+          amount={expenses[1].amount}
+          date={expenses[1].date}
+        ></ExpanseItem>
+        <ExpanseItem
+          title={expenses[3].title}
+          amount={expenses[3].amount}
+          date={expenses[3].date}
+        ></ExpanseItem>
       </header>
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,14 @@
 import "./ExpenseItem.css";
-function ExpanseItem() {
+function ExpanseItem(props) {
   const expenseDate = new Date(2022, 2, 28);
   const expenseTitle = "Car Insurance";
   const expenseAmount = 12.5;
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
